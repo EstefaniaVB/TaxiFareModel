91837ed44e698f07a501f784e5d95e845de06c2d
Estefania Vidal Bouzon <estefaniavidalbouzon@gmail.com>
2021-05-25 21:52:56 +0200

    TaxiFareModel mlflow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -6,8 +6,14 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+
+
 
 class Trainer():
+
     def __init__(self,X,y):
         """
             X: pandas DataFrame
@@ -16,10 +22,6 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
-        self.X_train = None
-        self.X_test=None
-        self.y_train =None
-        self.y_test  = None
         
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -38,9 +40,8 @@ class Trainer():
 
     def run(self):
         """set and train the pipeline"""
-        X_train, X_test, y_train, y_test = holdout(self.X,self.y)
         self.set_pipeline()
-        self.pipeline.fit(X_train, y_train)
+        self.pipeline.fit(self.X, self.y)
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
@@ -48,6 +49,30 @@ class Trainer():
         rmse = compute_rmse(y_pred, y_test)
         print(rmse)
         return rmse
+    
+    @memoized_property
+    def mlflow_client(self):
+        MLFLOW_URI = "https://mlflow.lewagon.co/"
+        EXPERIMENT_NAME = "[DE] [Berlin] [EstefaniaVB] TaxiFare1"
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -58,10 +83,10 @@ if __name__ == "__main__":
     # set X and y
     y = df["fare_amount"]
     X = df.drop("fare_amount", axis=1)
-    # train
-    trainer= Trainer(X,y)
-    trainer.run()
     X_train, X_test, y_train, y_test = holdout(X,y)
+    # train
+    trainer= Trainer(X_train,y_train)
+    trainer.run()
     # evaluate
     rmse =trainer.evaluate(X_test,y_test)
     print('rmse')
